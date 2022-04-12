466087c15bb4e91291f86959b775679ed2aaf81d
Kieran Ahn <kieranahn@gmail.com>
2022-04-12 04:23:39 -0700

    Updated compiler test for new functional option

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -11,11 +11,19 @@ const sampleProgram = `print("Hello, world!");`;
 
 describe("The compiler", () => {
     it("throws when the output type is unknown", (done) => {
-        assert.throws(() => compile("print(0);", "blah"), /Unknown output type/);
+        assert.throws(
+            () => compile("print(0);", "blah"),
+            /Unknown output type/
+        );
         done();
     });
     it("accepts the ast option", (done) => {
         const compiled = compile(sampleProgram, "ast");
+        assert(util.format(compiled).startsWith("   1 | Program"));
+        done();
+    });
+    it("accepts the analyzed option", (done) => {
+        const compiled = compile(sampleProgram, "analyzed");
         assert(util.format(compiled).startsWith("   1 | Program"));
         done();
     });
